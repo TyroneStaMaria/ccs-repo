0904a0737cfb03ae8dfe9adb0c34bcbe83107ac5
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-03-15 13:54:03 +0800

    established mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
+const mongoose = require("mongoose");
 
-app.listen(PORT, () => {
-  console.log("You are listening at port " + PORT);
-});
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    app.listen(PORT, () => {
+      console.log("You are listening at port " + PORT);
+    });
+  });
