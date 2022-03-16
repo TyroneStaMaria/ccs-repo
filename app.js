bd9305859c6434e419d39552e09bc2e7137cc2a4
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-03-16 21:10:20 +0800

    changed article route path

--- app.js
+++ app.js
@@ -27,5 +27,5 @@ app.engine("hbs", hbs.engine);
 app.use(express.static("public"));
 
 app.use(viewRoutes);
-app.use(`${API_ROUTE}/articles`, articleRoutes);
+app.use("/articles", articleRoutes);
 module.exports = app;
