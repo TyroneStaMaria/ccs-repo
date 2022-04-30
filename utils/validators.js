27e15a84a82c07d297bdb70570681f077726008f
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-04-30 14:53:11 +0800

    update validation rules

--- utils/validators.js
+++ utils/validators.js
@@ -1,9 +1,6 @@
 const { body } = require("express-validator");
 const loginValidators = [
-  body("email")
-    .isEmail()
-    .notEmpty()
-    .withMessage("Please Provide a valid Email"),
+  body("email").isEmail().withMessage("Please Provide a valid Email"),
   body("password").notEmpty().withMessage("Please Provide a password"),
 ];
 
