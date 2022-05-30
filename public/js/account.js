5e8a6d783cd22f65a17f5b0e6af68fb593cef17d
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-05-30 17:35:03 +0800

    add delete account modal

---
+++ public/js/account.js
@@ -0,0 +1,9 @@
+function openModal() {
+  const modal = document.querySelector(".delete-modal");
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  const modal = document.querySelector(".delete-modal");
+  modal.style.display = "none";
+}
