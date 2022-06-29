9201df756a815b085c8229d55edfcce1936060ff
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-06-30 00:26:03 +0800

    refactor modal changes

---
+++ public/js/modal.js
@@ -0,0 +1,10 @@
+function openModal() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = "flex";
+  return modal;
+}
+
+function closeModal() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = "none";
+}
